5d624a862515426149f132a8801819a48504b067
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-04-12 13:28:55 +0530

    connect to MongoDB Atlas

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 require("dotenv").config();
+const connectDB = require("./db/index.js");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running at port : ${PORT} on ${process.env.DEV_MODE} mode`);
-});
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running at port : ${PORT} on ${process.env.DEV_MODE} mode`);
+        });
+    })
+    .catch((err) => {
+        console.error("MONGO DB connection failed !!", err);
+    });
