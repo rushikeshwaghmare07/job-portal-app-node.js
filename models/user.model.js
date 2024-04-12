0eda24e82be234a9c6402bef6de847abb287e37f
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-04-12 13:48:41 +0530

    added user schema model

---
+++ models/user.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name Is Require"],
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: [true, " Email is Require"],
+      unique: true,
+      validate: validator.isEmail,
+    },
+    password: {
+      type: String,
+      required: [true, "password is require"],
+      minlength: [6, "Password length should be greater than 6 character"],
+      select: true,
+    },
+    location: {
+      type: String,
+      default: "India",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
