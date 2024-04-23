02f6168d3fa4e7a4649d742f2e059f03b30c1bc3
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-04-23 10:21:41 +0530

    update error middleware

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,6 +1,18 @@
 const errorMiddleware = (err, req, res, next) => {
-    console.error(err)
-    res.status(500).json({ success: false, message: "Something went wrong" });
-}
+    console.error(err);
+
+    let statusCode = 500;
+    let message = "Internal Server Error";
+
+    if (err.name === "ValidationError") {
+        statusCode = 400;
+        message = Object.values(err.errors).map(item => item.message).join(", ");
+    } else if (err.code === 11000) {
+        statusCode = 400;
+        message = `${Object.keys(err.keyValue)} field has to be unique`;
+    }
+
+    res.status(statusCode).json({ message });
+};
 
 module.exports = errorMiddleware;
