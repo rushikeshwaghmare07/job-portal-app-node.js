3d74898613064c5fd36b5572f7c53156759d5ba0
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-04-25 20:31:00 +0530

    added response message in auth middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -4,14 +4,16 @@ const userAuth = async (req, res, next) => {
     try {
         const authHeader = req.headers.authorization;
         if (!authHeader || !authHeader.startsWith("Bearer")) {
-            throw new Error("Authentication Failed: No Bearer token provided");
+            // next("Authentication Failed: No Bearer token provided");
+            res.status(400).json({ success: false, message: "Authentication Failed: No Bearer token provided"})
+
         }
         const token = authHeader.split(" ")[1];
         const payload = JWT.verify(token, process.env.JWT_SECRET);
         req.user = { userId: payload.userId };
         next();
     } catch (error) {
-        next("Authentication Failed: ", error);
+        next("Authentication Failed: ");
     }
 };
 
