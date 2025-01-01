ccfb1ed5e42e245024e1cd66aaeb258075039cba
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-01 22:33:38 +0530

    Update user register controller 'add error check'

--- controllers/authenticate.controller.js
+++ controllers/authenticate.controller.js
@@ -1,50 +1,74 @@
 const User = require("../models/user.model.js");
 
 const registerUserController = async (req, res, next) => {
-    try {
-      const { name, email, password } = req.body;
-  
-      const existingUser = await User.findOne({ email });
-      if (existingUser) {
-        return res.status(400).json({ success: false, message: "Email is already registered. Please log in instead." });
-      }
-  
-      const newUser = await User.create({ name, email, password });
-  
-      // token
-      const token = await newUser.createJWT();
+  try {
+    const { name, email, password } = req.body;
 
-      return res.status(201).json({ success: true, message: "User created successfully, Please log in.", newUser, token });
-    } catch (error) {
-        if (error.name === 'ValidationError') {
-            const errorMessage = Object.values(error.errors)[0].message;
-            return res.status(400).json({ success: false, message: errorMessage });
-        }
-        next(error);
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required." });
     }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(409)
+        .json({ success: false, message: "Email is already registered. Please log in instead." });
+    }
+
+    const newUser = await User.create({ name, email, password });
+
+    // token
+    const token = await newUser.createJWT();
+
+    return res
+      .status(201)
+      .json({
+        success: true,
+        message: "User created successfully, Please log in.",
+        newUser,
+        token,
+      });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      const errorMessage = Object.values(error.errors)[0].message;
+      return res.status(400).json({ success: false, message: errorMessage });
+    }
+    next(error);
+  }
 };
 
 const loginUserController = async (req, res, next) => {
   try {
     const { email, password } = req.body;
+
     if (!email || !password) {
-      return res.status(400).json({ success: false, message: "Please Provide All Fields" });
-    };
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required." });
+    }
 
     const user = await User.findOne({ email }).select("+password");
     if (!user) {
-        return res.status(401).json({ success: false, message: "Invalid Username or Password" });
-    };
-  
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid Username or Password" });
+    }
+
     //compare password
     const isMatch = await user.comparePassword(password);
     if (!isMatch) {
-      return res.status(401).json({ success: false, message: "Invalid Username or Password" });
-    };
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid Username or Password" });
+    }
 
     user.password = undefined;
     const token = user.createJWT();
-    return res.status(200).json({ success: true, message: "Login Successfully", user, token });
+    return res
+      .status(200)
+      .json({ success: true, message: "Login Successfully", user, token });
   } catch (error) {
     next(error);
   }
@@ -52,5 +76,5 @@ const loginUserController = async (req, res, next) => {
 
 module.exports = {
   registerUserController,
-  loginUserController
+  loginUserController,
 };
